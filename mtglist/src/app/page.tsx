bf015303f08081785b5b7cd771aa1799d2409009
IyXuan23 <127928542+IyXuan23@users.noreply.github.com>
2025-01-02 04:46:12 +1100

    removed unncessary variables
    removed geist and geist_mono from global layout
    removed unnecessary sidenav import from app/page.tsx

--- mtglist/src/app/page.tsx
+++ mtglist/src/app/page.tsx
@@ -1,20 +1,7 @@
 import React from 'react';
-import SideNav from './ui/sidenav';
 import WebsiteLogo from './ui/websiteLogo';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
-
-
-// export default function Home({children}: {children: React.ReactNode}) {
-//   return (
-//     <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
-//       <div className='w-full flex-none md:w-64'>
-//         <SideNav/>
-//       </div>
-//       <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>{children}</div>
-//     </div>
-//   )<
-// }
 
 export default function Home() {
 
