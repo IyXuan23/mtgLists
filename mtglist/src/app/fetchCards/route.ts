a8a28e8733f524d9efa85367fece299ec042393a
IyXuan23 <127928542+IyXuan23@users.noreply.github.com>
2025-01-09 19:55:29 +1100

    Seeded the tables and inserted data
    Seeded the tables for the database and inserted the data for cards and deck. However, have not linked the cards using card-deck yet, will probably make a UI for that

---
+++ mtglist/src/app/fetchCards/route.ts
@@ -0,0 +1,156 @@
+import {writeFileSync} from "fs";
+
+export async function GET() {
+
+    //cardList
+    const cardList = ['gonti, canny acquisitor',
+        'forest',
+        'llanowar wastes',
+        'fellwar stone',
+        'yavimaya coast',
+        'arcane signet',
+        'necroblossom snarl',
+        'swamp',
+        'hinterland harbor',
+        'orochi soul-reaver',
+        'twilight mire',
+        'ghostly pilferer',
+        'commander\'s sphere',
+        'stolen goods',
+        'mercurial spelldancer',
+        'edric, spymaster of trest',
+        'dream-theif\'s bandana',
+        'malcolm, alluring scoundrel',
+        'rampant growth',
+        'brainstealer dragon',
+        'island',
+        'gonti, lord of luxury',
+        'thief of sanity',
+        'foreboding landscape',
+        'drowned catacomb',
+        'exotic orchard',
+        'vineglimmer snarl',
+        'rogue\'s passage',
+        'changeling outcast',
+        'heartless conscription',
+        'underground river',
+        'villainous wealth',
+        'temple of malady',
+        'disorienting choice',
+        'come back wrong',
+        'doc aurlock, grizzled genius',
+        'slither blade',
+        'coastal piracy',
+        'nature\'s lore',
+        'oblivion sower',
+        'malleable impostor',
+        'predators\ hour',
+        'nashi, moon sage\'s scion',
+        'the mimeoplasm',
+        'baleful mastery',
+        'cold-eyed selkie',
+        'arcane heist',
+        'farseek',
+        'felix five-boots',
+        'kodama\'s reach',
+        'invisible stalker',
+        'opulent palace',
+        'tasha, the witch queen',
+        'chromatic lantern',
+        'cultivate',
+        'ohran frostfang',
+        'triton shorewalker',
+        'siphon insight',
+        'savvy trader',
+        'hostage taker',
+        'thieving skydiver',
+        'reconnaissance mission',
+        'temple of mystery',
+        'thieving amalgam',
+        'valgavoth, terror eater',
+        'fallen shinobi',
+        'counterspell',
+        'sunken hollow',
+        'smirking spelljacker',
+        'toxic deluge',
+        'three visits',
+        'putrefy',
+        'tower winder',
+        'baleful strix',
+        'thieving varmint',
+        'shadowmage infiltrator',
+        'flooded grove',
+        'silhana ledgewalker',
+        'darkslick shores',
+        'reliquary tower',
+        'sol ring',
+        'temple of deceit',
+        'woodland cemetery',
+        'command tower',
+        'feed the swarm',
+        'silent blade oni',
+        'mind\'s dilation',
+        'phyrexian arena'
+    ]
+
+    try {
+        const response = await fetchCardAndSave(cardList);
+
+        if (!response) {
+            throw new Error('failed after exiting fetchcard function');
+        }
+
+        return new Response(JSON.stringify({message: 'Card fetched successfully', data: response}), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+
+    catch (error) {
+        return new Response(JSON.stringify({error: (error as Error).message}), {
+            status: 500,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+}
+
+async function fetchCardAndSave(cardNames: string[]) {
+
+    //fetch card from scryfall
+    //we shall test with gonti canny acquisitor first
+
+    const allCardData = []
+
+    for (let cardName of cardNames) {
+
+        const res = await fetch(`https://api.scryfall.com/cards/named?fuzzy=${cardName}`);
+
+        if (!res.ok) {
+            throw new Error('failed to fetch from scryfall API');
+        }
+
+        const cardData = await res.json();
+
+        const importantData = {
+            name: cardData.name,
+            small_image_uri: cardData.image_uris.small,
+            normal_image_uri: cardData.image_uris.normal,
+            cmc: cardData.cmc,
+            type_line: cardData.type_line
+        };
+
+        allCardData.push(importantData)
+        console.log(`${cardData.name} retrieved`);
+
+    }
+
+    const fileName = './all_card_data.json'
+    writeFileSync(fileName, JSON.stringify(allCardData, null, 2));
+    console.log('file complete')
+
+    return {job: 'complete'};
+}
--- mtglist/src/app/seed/route.ts
+++ mtglist/src/app/seed/route.ts
@@ -1,14 +1,15 @@
 import { db } from "@vercel/postgres";
 
+
 export async function GET() {
     try {
-        const response = await fetchCard("gonti, canny acquisitor");
+        const response = await seedTable();
 
         if (!response) {
-            throw new Error('failed after exiting fetchcard function');
+            throw new Error('error failed to seed tables into database');
         }
 
-        return new Response(JSON.stringify({message: 'Card fetched successfully', data: response}), {
+        return new Response(JSON.stringify({message: 'Database Tables Created', data: response}), {
             status: 200,
             headers: {
                 'Content-Type': 'application/json'
@@ -26,46 +27,25 @@ export async function GET() {
     }
 }
 
+async function seedTable() {
 
-async function fetchCard(cardName: string) {
-
-    //fetch card from scryfall
-    //we shall test with gonti canny acquisitor first
-    const res = await fetch(`https://api.scryfall.com/cards/named?fuzzy=${cardName}`);
-
-    if (!res.ok) {
-        throw new Error('failed to fetch from scryfall API');
-    }
-
-    const cardData = await res.json();
-
-    const importantData = {
-        name: cardData.name,
-        small_image_uri: cardData.image_uris.small,
-        normal_image_uri: cardData.image_uris.normal,
-        cmc: cardData.cmc,
-        type_line: cardData.type_line
-    };
-
-    console.log(importantData);
-
-    return importantData;
-}
-
-async function seedTableAndCards() {
     const client = await db.connect();
 
     await client.sql `CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
+    console.log('extension created');
+
     await client.sql `CREATE TABLE IF NOT EXISTS cards (
-        card_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY
-        name VARCHAR(255) NOT NULL
-        cmc VARCHAR(50) NOT NULL
-        small_image_uri VARCHAR(255)
-        normal_image_Uri VARCHAR(255)
+        card_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        card_name VARCHAR(255) NOT NULL,
+        cmc VARCHAR(50) NOT NULL,
+        small_image_uri VARCHAR(255),
+        normal_image_uri VARCHAR(255),
         type_line VARCHAR(255)
         );
     `;
+
+    console.log('cards table created');
 
     await client.sql `CREATE TABLE IF NOT EXISTS deck (
         deck_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
@@ -73,13 +53,19 @@ async function seedTableAndCards() {
         );
     `;
 
+    console.log('deck table created');
+
     await client.sql `CREATE TABLE IF NOT EXISTS cards_deck (
-        deck_id UUID
-        card_id UUID
+        deck_id UUID,
+        card_id UUID,
         quantity INT DEFAULT 1,
-        PRIMARY KEY (deck_id, card_id)
-        FOREIGN KEY (deck_id) REFERENCES deck(deck_id) ON DELETE CASCADE
-        FOREIGN KEY (card_id) REFERENCES card(card_id) ON DELETE CASCADE
+        PRIMARY KEY (deck_id, card_id),
+        FOREIGN KEY (deck_id) REFERENCES deck(deck_id) ON DELETE CASCADE,
+        FOREIGN KEY (card_id) REFERENCES cards(card_id) ON DELETE CASCADE
         );
     `;
+
+    console.log('card-deck table created');
+
+    return {database: 'created'};
 }
