1327968d89fc7d527d15776eb42a4b3870609304
IyXuan23 <127928542+IyXuan23@users.noreply.github.com>
2025-01-11 14:49:06 +1100

    finished off the edit banner
    added edit and delete buttons
    
    Delete button is currently not linked up to anything
    Edit button redirects to /edit page for the specific deck

--- mtglist/src/app/ui/editbanner.tsx
+++ mtglist/src/app/ui/editbanner.tsx
@@ -1,9 +1,13 @@
 'use client'
 
-import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/react"
-import { ChevronDownIcon, PencilIcon } from "@heroicons/react/16/solid";
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/react";
+import { ChevronDownIcon, PencilIcon, TrashIcon } from "@heroicons/react/16/solid";
+import Link from 'next/link';
+import { usePathname } from "next/navigation";
 
 export default function EditBanner() {
+
+    const pathname = usePathname();
 
     return(
         <div className="flex justify-end p-4">
@@ -15,15 +19,23 @@ export default function EditBanner() {
                 </MenuButton>
                 <MenuItems transition
                 anchor='bottom end'
-                className='w-52 origin-top-right rounded-md border border-white/10 bg-white/10 p-1'>
+                className='w-52 origin-top-right rounded-md border border-black/50 bg-gray-400/90 p-1 transition'>
 
                     <MenuItem>
-                        <button className="flex w-full group rounded-md bg-gray items-center gap-2 py-1.5 px-3 data-[focus]:bg-white/10">
+                        <Link className="flex w-full group rounded-md bg-gray items-center gap-2 py-1.5 px-3 data-[focus]:bg-white/10"
+                            href={pathname + '/edit'}>
                             Edit
                             <PencilIcon className="size-4"/>
+                        </Link>
+                    </MenuItem>
+                    <MenuItem>
+                        <button className="flex w-full group rounded-md bg-gray items-center gap-2 py-1.5 px-3 data-[focus]:bg-white/10">
+                            Delete
+                            <TrashIcon className="size-4"/>
                         </button>
                     </MenuItem>
                 </MenuItems>
+                
 
             </Menu>
         </div>
